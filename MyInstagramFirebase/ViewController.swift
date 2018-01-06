c352d3adc43dc5c08e6ee91bb737531a2a4b3ebf
Oluwatobi Omotayo <tobitech@ymail.com>
2018-01-06 15:07:16 +0100

    save username to firebase storage, add image picker controller to the signup screen

--- MyInstagramFirebase/ViewController.swift
+++ MyInstagramFirebase/ViewController.swift
@@ -9,13 +9,41 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
+        
         return button
     }()
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     let emailTextField: UITextField = {
         let textField = UITextField()
@@ -23,6 +51,7 @@ class ViewController: UIViewController {
         textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 14)
+        textField.autocapitalizationType = .none
         
         textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         
@@ -48,7 +77,7 @@ class ViewController: UIViewController {
         textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 14)
-        
+        textField.autocapitalizationType = .none
         textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         
         return textField
@@ -97,6 +126,23 @@ class ViewController: UIViewController {
             
             print("Successfully create user: ", user?.uid ?? "")
             
+            /// execute code that will save username into Firebase Database
+            
+            guard let uid = user?.uid else { return }
+            
+            let usernameValues = ["username": username]
+            let values = [uid:usernameValues]
+            
+            Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                
+                if let err = err {
+                    print("Failed to save user info into db: ", err)
+                    return
+                }
+                
+                print("Successfully saved user info to db")
+                
+            })
         }
     }
 
