c387e26e441540f90f4490e036d81e3f7ed7d536
Oluwatobi Omotayo <tobitech@ymail.com>
2018-01-08 10:13:06 +0100

    move tab bar items down to be centred in the tab bar

--- MyInstagramFirebase/MainTabBarController.swift
+++ MyInstagramFirebase/MainTabBarController.swift
@@ -54,6 +54,14 @@ class MainTabBarController: UITabBarController {
                            plusNavController,
                            likeNavController,
                            userProfileNavController]
+        
+        // modify tab bar items insets
+        guard let items = tabBar.items else { return }
+        
+        for item in items {
+            item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+        }
+        
     }
     
     fileprivate func templateNavController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController = UIViewController()) -> UINavigationController {
