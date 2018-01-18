231db5fee21b122fd48613270778a8dca77f5d5f
Oluwatobi Omotayo <tobitech@ymail.com>
2018-01-18 14:39:21 +0100

    implement an alert with animation and a uilabel, with CATransform3Dmake to show user that photo has been saved

--- MyInstagramFirebase/Camera/PreviewPhotoContainerView.swift
+++ MyInstagramFirebase/Camera/PreviewPhotoContainerView.swift
@@ -50,6 +50,40 @@ class PreviewPhotoContainerView: UIView {
             }
             
             print("Successfully saved image to photo libray")
+            
+            DispatchQueue.main.async {
+                let savedLabel = UILabel()
+                savedLabel.text = "Saved Successfully"
+                savedLabel.textColor = .white
+                savedLabel.font = UIFont.boldSystemFont(ofSize: 18)
+                savedLabel.numberOfLines = 0
+                savedLabel.backgroundColor = UIColor(white: 0, alpha: 0.3)
+                savedLabel.textAlignment = .center
+                
+                savedLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 80)
+                savedLabel.center = self.center
+                
+                self.addSubview(savedLabel)
+                
+                savedLabel.layer.transform = CATransform3DMakeScale(0, 0, 0)
+                
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    
+                    savedLabel.layer.transform = CATransform3DMakeScale(1, 1, 1)
+                    
+                }, completion: { (completed) in
+                    // completed
+                    UIView.animate(withDuration: 0.5, delay: 0.75, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                        
+                        savedLabel.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+                        savedLabel.alpha = 0
+                        
+                    }, completion: { (_) in
+                        savedLabel.removeFromSuperview()
+                    })
+                })
+            }
+            
         }
     }
     
