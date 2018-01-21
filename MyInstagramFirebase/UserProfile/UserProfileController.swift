70166041419557bb9da1f5342f197eb48d860a5d
Oluwatobi Omotayo <tobitech@ymail.com>
2018-01-21 06:20:44 +0100

    implement the right pagination ordering by creation date, reversing the snapshot objects so that i can paginate upwards

--- MyInstagramFirebase/UserProfile/UserProfileController.swift
+++ MyInstagramFirebase/UserProfile/UserProfileController.swift
@@ -41,22 +41,25 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         
         let ref = Database.database().reference().child("posts").child(uid)
         
-        var query = ref.queryOrderedByKey()
+//        var query = ref.queryOrderedByKey()
+        var query = ref.queryOrdered(byChild: "creationDate")
         
         if posts.count > 0 {
-            let value = posts.last?.id
-            query = query.queryStarting(atValue: value)
+            let value = posts.last?.creationDate.timeIntervalSince1970
+            query = query.queryEnding(atValue: value)
         }
         
-        query.queryLimited(toFirst: 4).observeSingleEvent(of: .value, with: { (snapshot) in
+        query.queryLimited(toLast: 4).observeSingleEvent(of: .value, with: { (snapshot) in
             
             guard var allObjects = snapshot.children.allObjects as? [DataSnapshot] else { return }
+            
+            allObjects.reverse()
             
             if allObjects.count < 4 {
                 self.isFinishedPaging = true
             }
             
-            if self.posts.count > 0 {
+            if self.posts.count > 0 && allObjects.count > 0 {
                 allObjects.removeFirst()
             }
             
